d0b7801fb704b45d2822ea992fdddf9d79924552
Yonsh Lin <yonsh@live.com>
2020-12-09 16:12:42 +0800

    Initial commit

---
+++ Assets/Scripts/PluginTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PluginTest : MonoBehaviour
+{
+    private void Start()
+    {
+        var jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        var activity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        var signin = new AndroidJavaObject("com.omnibit.loginmodule.GoogleSignInFragment");
+        signin.SetStatic("UnityGameObjectName", name);
+        signin.CallStatic("SignIn", activity, "CLIENT_ID");
+    }
+
+    public void UnityGoogleSignInSuccessCallback(string e)
+    {
+        Debug.Log("Sign In Success");
+        Debug.LogWarning(e);
+    }
+
+    public void UnityGoogleSignInErrorCallback(string e)
+    {
+        Debug.Log("Sign In Failed");
+        Debug.LogError(e);
+    }
+}
